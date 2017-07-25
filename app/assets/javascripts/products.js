8cfc16383d58037569b738ec101ac54cd4aff99d
Alex Kalinin <login.hedin@gmail.com>
2017-07-26 01:40:25 +0600

    Fix bug

--- app/assets/javascripts/products.js
+++ app/assets/javascripts/products.js
@@ -1,4 +1,4 @@
-window['products#edit'] = function(){
+window['products#edit'] = window['products#new'] = function(){
   document.addEventListener("turbolinks:load", function () {
     var input = document.getElementById("product_name");
     new Awesomplete(input, {
